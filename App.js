731d60768c1b17b27b4e1a15bd006ed784092327
pohjie <pohjie.pj@gmail.com>
2021-09-27 19:21:40 +0800

    setup nav structure

--- App.js
+++ App.js
@@ -1,30 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-import HomeScreen from './screens/HomeScreen';
-import LoggedScreen from './screens/LoggedScreen';
-import RecordScreen from './screens/RecordScreen';
-import MotivationScreen from './screens/MotivationScreen';
-
-const Stack = createNativeStackNavigator();
-const Tab = createBottomTabNavigator();
-
+import { MainStackNavigator } from './stacks/MainStackNavigator';
+import { BottomTabNavigator } from './stacks/MainTabNavigator';
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Logged" component={LoggedScreen} />
-      </Stack.Navigator>
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Record" component={RecordScreen} />
-        <Tab.Screen name="Motivation" component={MotivationScreen} />
-      </Tab.Navigator>
+      <BottomTabNavigator />
     </NavigationContainer>
   );
 }
---
+++ stacks/MainTabNavigator.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import { MainStackNavigator } from './MainStackNavigator';
+import RecordScreen from '../screens/RecordScreen';
+import MotivationScreen from '../screens/MotivationScreen';
+
+const Tab = createBottomTabNavigator();
+
+const BottomTabNavigator = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Deep Work Tracker" component={MainStackNavigator} />
+      <Tab.Screen name="Record" component={RecordScreen} />
+      <Tab.Screen name="Motivation" component={MotivationScreen} />
+    </Tab.Navigator>
+  );
+};
+
+export { BottomTabNavigator }
